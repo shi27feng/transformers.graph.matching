5d1f5ad6f2f4831abde378cd310625d31125de17
Feng Shi <shi.feng@cs.ucla.edu>
2021-05-03 07:41:56 -0700

    Create optimizer.py

---
+++ src/optimizer.py
@@ -0,0 +1,104 @@
+import torch
+from torch.optim.optimizer import Optimizer, required
+
+
+def unitwise_norm(x: torch.Tensor):
+    if x.ndim <= 1:
+        dim = 0
+        keepdim = False
+    elif x.ndim in [2, 3]:
+        dim = 0
+        keepdim = True
+    elif x.ndim == 4:
+        dim = [1, 2, 3]
+        keepdim = True
+    else:
+        raise ValueError('Wrong input dimensions')
+
+    return torch.sum(x ** 2, dim=dim, keepdim=keepdim) ** 0.5
+
+
+class SGDwAGC(Optimizer):
+    def __init__(self, params, lr=required, momentum=0, dampening=0,
+                 weight_decay=0, nesterov=False, clipping=1e-2, eps=1e-3):
+        if lr is not required and lr < 0.0:
+            raise ValueError("Invalid learning rate: {}".format(lr))
+        if momentum < 0.0:
+            raise ValueError("Invalid momentum value: {}".format(momentum))
+        if weight_decay < 0.0:
+            raise ValueError(
+                "Invalid weight_decay value: {}".format(weight_decay))
+        if clipping < 0.0:
+            raise ValueError("Invalid clipping value: {}".format(clipping))
+        if eps < 0.0:
+            raise ValueError("Invalid eps value: {}".format(eps))
+
+        defaults = dict(lr=lr, momentum=momentum, dampening=dampening,
+                        weight_decay=weight_decay, nesterov=nesterov, clipping=clipping, eps=eps)
+        if nesterov and (momentum <= 0 or dampening != 0):
+            raise ValueError(
+                "Nesterov momentum requires a momentum and zero dampening")
+        super(SGDwAGC, self).__init__(params, defaults)
+
+    def __setstate__(self, state):
+        super(SGDwAGC, self).__setstate__(state)
+        for group in self.param_groups:
+            group.setdefault('nesterov', False)
+
+    @torch.no_grad()
+    def step(self, closure=None):
+        """Performs a single optimization step.
+
+        Arguments:
+            closure (callable, optional): A closure that reevaluates the model
+                and returns the loss.
+        """
+        loss = None
+        if closure is not None:
+            with torch.enable_grad():
+                loss = closure()
+
+        for group in self.param_groups:
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+                param_norm = torch.max(unitwise_norm(
+                    p.detach()), torch.tensor(group['eps']).to(p.device))
+                grad_norm = unitwise_norm(p.grad.detach())
+                max_norm = param_norm * group['clipping']
+
+                trigger = grad_norm > max_norm
+
+                clipped_grad = p.grad * \
+                    (max_norm / torch.max(grad_norm,
+                                          torch.tensor(1e-6).to(grad_norm.device)))
+                p.grad.detach().copy_(torch.where(trigger, clipped_grad, p.grad))
+
+        for group in self.param_groups:
+            weight_decay = group['weight_decay']
+            momentum = group['momentum']
+            dampening = group['dampening']
+            nesterov = group['nesterov']
+
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+                d_p = p.grad
+                if weight_decay != 0:
+                    d_p = d_p.add(p, alpha=weight_decay)
+                if momentum != 0:
+                    param_state = self.state[p]
+                    if 'momentum_buffer' not in param_state:
+                        buf = param_state['momentum_buffer'] = torch.clone(
+                            d_p).detach()
+                    else:
+                        buf = param_state['momentum_buffer']
+                        buf.mul_(momentum).add_(d_p, alpha=1 - dampening)
+                    if nesterov:
+                        d_p = d_p.add(buf, alpha=momentum)
+                    else:
+                        d_p = buf
+
+                p.add_(d_p, alpha=-group['lr'])
+
+        return loss
